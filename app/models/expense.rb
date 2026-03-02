2402296bd46f6fb063aed0a3df714aca9f8bd706
Shashi Bhushan Kumar <shashi@apples-MacBook-Pro.local>
2026-03-02 16:25:26 +0530

    Added some validations and associations in category, expenses , recceipts

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,8 +1,9 @@
 class Expense < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :receipts, dependent: :destroy
 
   enum :status, { pending: 0, approved: 1, rejected: 2 }
 
-  validates :amount, :expense_date, presence: true
+  validates :amount, :expense_date, :category, presence: true
 end
--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,3 +1,6 @@
 class Receipt < ApplicationRecord
   belongs_to :expense
+
+  validates :file_url, :file_name, :file_type, :amount, :receipt_date, presence: true
+  validates :amount, numericality: { greater_than: 0 }
 end
