794a9adfd67e242116681594bd4011e94b65fac6
Shashi Bhushan Kumar <shashi@apples-MacBook-Pro.local>
2026-03-03 13:21:17 +0530

    file name changed because of rails loads the singular serializer not serializers

--- app/serializers/expense_serializers.rb
+++
@@ -1,7 +0,0 @@
-class ExpenseSerializer
-  include Alba::Resource
-  attributes :id, :amount, :description, :expense_date, :status, :resolved_at
-  association :user, serializer: UserSerializer
-  association :category, serializer: CategorySerializer
-  association :receipts, serializer: ReceiptSerializer
-end
