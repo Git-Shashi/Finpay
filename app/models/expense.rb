4b6c0dac68cc3415bcc430facfc34b0f230f84cf
Shashi Bhushan Kumar <shashi@apples-MacBook-Pro.local>
2026-03-09 12:41:38 +0530

    added scopes for chaining query and reusablity  and validation of min_ammount

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -6,4 +6,9 @@ class Expense < ApplicationRecord
   enum :status, { pending: 0, approved: 1, rejected: 2 }
 
   validates :amount, :expense_date, presence: true
+  validates :amount, numericality: { greater_than: 0 }
+  scope :by_category, ->(category_id) { where(category_id: category_id) }
+  scope :by_status, ->(status) { where(status: status) }
+  scope :by_date_range, ->(start_date, end_date) { where(expense_date: start_date..end_date) }
+
 end
