351d0867dadae99c92b4681b67207b4c407474c3
Shashi Bhushan Kumar <shashi@apples-MacBook-Pro.local>
2026-03-10 12:33:30 +0530

    updated expense serializers

--- app/serializers/expense_serializer.rb
+++ app/serializers/expense_serializer.rb
@@ -1,7 +1,12 @@
 class ExpenseSerializer
   include Alba::Resource
 
-  attributes :id, :amount, :description, :expense_date, :status, :resolved_at
+  attributes :id, :amount, :description, :expense_date, :aasm_state, :resolved_at
+
+  attribute :available_transitions do |expense|
+    expense.aasm.events(permitted: true).map(&:name)
+  end
+
   association :user, serializer: UserSerializer
   association :category, serializer: CategorySerializer
   association :receipts, serializer: ReceiptSerializer
