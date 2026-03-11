6361bd2311781f58107892463e6a80fff2e2f8e9
Shashi Bhushan Kumar <shashi@apples-MacBook-Pro.local>
2026-03-11 13:25:34 +0530

    Removed unnecessary transition details in serilizer , That was being passed to the frontend

--- app/serializers/expense_serializer.rb
+++ app/serializers/expense_serializer.rb
@@ -3,9 +3,7 @@ class ExpenseSerializer
 
   attributes :id, :amount, :description, :expense_date, :aasm_state, :resolved_at
 
-  attribute :available_transitions do |expense|
-    expense.aasm.events(permitted: true).map(&:name)
-  end
+  
 
   association :user, serializer: UserSerializer
   association :category, serializer: CategorySerializer
