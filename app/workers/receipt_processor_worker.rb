716efc1bf534c5ec423da652fc4a5083cc5c56a9
Shashi Bhushan Kumar <shashi@apples-MacBook-Pro.local>
2026-03-13 16:37:02 +0530

    Added tenant switching logic for recceipt processor worker

--- app/workers/receipt_processor_worker.rb
+++ app/workers/receipt_processor_worker.rb
@@ -1,9 +1,18 @@
 class ReceiptProcessorWorker
   include Sidekiq::Worker
 
-  def perform(receipt_id)
-    receipt = Receipt.find(receipt_id)
-    # Add your processing logic here
-    receipt.process!
+  def perform(receipt_id, tenant = nil)
+    if tenant.present?
+      Apartment::Tenant.switch(tenant) do
+        receipt = Receipt.find(receipt_id)
+        receipt.process!
+      end
+    else
+      receipt = Receipt.find(receipt_id)
+      receipt.process!
+    end
+  rescue StandardError => e
+    Rails.logger.error("ReceiptProcessorWorker failed: #{e.message}")
+    raise
   end
 end
