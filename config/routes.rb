20af5b3549a7dc2f9805b1aefda70dfed6e19e56
Shashi Bhushan Kumar <shashi@apples-MacBook-Pro.local>
2026-03-13 16:35:05 +0530

    Added authentication logic for sidekiq web ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 require "sidekiq/web"
 
+Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  username == ENV.fetch('SIDEKIQ_USERNAME', 'admin') &&
+    password == ENV.fetch('SIDEKIQ_PASSWORD', 'password')
+end
+
 Rails.application.routes.draw do
   # Devise Token Auth (ONLY authentication system)
   mount_devise_token_auth_for 'User', at: 'auth'
@@ -17,12 +22,13 @@ Rails.application.routes.draw do
       post :reject
       post :reimburse
       post :archive
+
+      # Receipt routes handled by ExpensesController
+      get  :receipts,       action: :receipts
+      post :receipts,       action: :create_receipt
+      delete 'receipts/:receipt_id', action: :destroy_receipt
     end
-
-    resources :receipts, only: [:index, :create, :destroy]
   end
 
-  authenticate :user, ->(u) { u.admin? } do
-    mount Sidekiq::Web => '/sidekiq'
-  end
+  mount Sidekiq::Web => '/sidekiq'
 end
