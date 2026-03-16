93ba6414e96bc90475679ac368be053450eb990a
Shashi Bhushan Kumar <shashi@apples-MacBook-Pro.local>
2026-03-16 15:33:37 +0530

    added ApplicationService base class, services now inherit it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include ErrorHandler
+  include ApiResponder
 
   before_action :configure_permitted_parameters, if: :devise_token_auth_controller?
 
