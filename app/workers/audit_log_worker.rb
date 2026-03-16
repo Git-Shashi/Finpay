7a4c0d6ddf097bbc93961ba798ee3e06a69dd3c2
Shashi Bhushan Kumar <shashi@apples-MacBook-Pro.local>
2026-03-16 15:44:25 +0530

    Refactor sidekiq worker to use the tenant loader's with_tenant method

--- app/workers/audit_log_worker.rb
+++ app/workers/audit_log_worker.rb
@@ -1,17 +1,16 @@
 class AuditLogWorker
   include Sidekiq::Worker
+  include TenantLoader
 
   sidekiq_options retry: 5
 
   def perform(expense_id, action, tenant)
-    raise ArgumentError, I18n.t("audit_log_worker.errors.tenant_required") if tenant.blank?
-
-    Apartment::Tenant.switch(tenant) do
+    with_tenant(tenant) do
       expense = Expense.find(expense_id)
       ActivityLog.create!(expense: expense, action: action)
     end
   rescue StandardError => e
-    Rails.logger.error((I18n.t("audit_log_worker.errors.failed", message: e.message)))
+    Rails.logger.error(I18n.t("errors.worker_failed", worker: self.class.name, message: e.message))
     raise
   end
 end
--- app/workers/receipt_processor_worker.rb
+++ app/workers/receipt_processor_worker.rb
@@ -1,16 +1,15 @@
 class ReceiptProcessorWorker
   include Sidekiq::Worker
+  include TenantLoader
 
   def perform(expense_id, receipt_id, tenant)
-    raise ArgumentError, I18n.t("receipt_processor_worker.errors.tenant_required") if tenant.blank?
-
-    Apartment::Tenant.switch(tenant) do
+    with_tenant(tenant) do
       expense = Expense.find(expense_id)
       receipt = expense.receipts.find(receipt_id)
       receipt.process!
     end
   rescue StandardError => e
-    Rails.logger.error((I18n.t("receipt_processor_worker.errors.failed", message: e.message)))
+    Rails.logger.error(I18n.t("errors.worker_failed", worker: self.class.name, message: e.message))
     raise
   end
 end
