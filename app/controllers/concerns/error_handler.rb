3698a80bd68a7a34b23c20abed1c5119b98bae19
Shashi Bhushan Kumar <shashi@apples-MacBook-Pro.local>
2026-03-16 15:48:07 +0530

    added error_handler concern

---
+++ app/controllers/concerns/error_handler.rb
@@ -0,0 +1,21 @@
+module ErrorHandler
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render json: { error: e.message }, status: :not_found
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    rescue_from UnauthorizedError do |e|
+      render json: { error: e.message }, status: :forbidden
+    end
+
+    rescue_from ActionController::ParameterMissing do |e|
+      render json: { error: e.message }, status: :bad_request
+    end
+  end
+end
