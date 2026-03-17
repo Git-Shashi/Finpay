ba568b35b45dfa80db83e159f4c0ea9be2d485d9
Git-Shashi <shashi847305@iiitmanipur.ac.in>
2026-03-17 18:29:36 +0530

    Added the secure headers in the Base controller that is sent with each response to the browsers

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,18 @@
 module Api
   module V1
     class BaseController < ApplicationController
+      before_action :set_secure_headers
+
+      private
+
+      def set_secure_headers
+        response.set_header('X-Content-Type-Options', 'nosniff')
+        response.set_header('X-Frame-Options', 'DENY')
+        response.set_header('X-XSS-Protection', '1; mode=block')
+        response.set_header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains')
+        response.set_header('Referrer-Policy', 'strict-origin-when-cross-origin')
+        response.set_header('Cache-Control', 'no-store')
+      end
     end
   end
 end
