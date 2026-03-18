8a7dcb27fb76d328e4e82e9b9ad9a4a2a7d5ed31
Git-Shashi <shashi847305@iiitmanipur.ac.in>
2026-03-18 10:27:01 +0530

    Add Rswag engines for API documentation

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Sidekiq::Web.use Rack::Auth::Basic do |username, password|
 end
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
