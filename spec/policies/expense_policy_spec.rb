ac6cd328730313aee597ce7a50c657c00bbb36d4
Git-Shashi <shashi847305@iiitmanipur.ac.in>
2026-03-23 17:49:47 +0530

    Added some spec for expense policy

--- spec/policies/expense_policy_spec.rb
+++ spec/policies/expense_policy_spec.rb
@@ -10,7 +10,28 @@ RSpec.describe ExpensePolicy, type: :policy do
   end
 
   permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+   context "when the user is the owner of the expense" do
+      let(:expense) { Expense.new(user: user) }
+
+      it "allows access" do
+        expect(subject).to permit(user, expense)
+      end
+    end
+    context "when the user is the admin" do
+      let(:admin) { User.new(admin: true) }
+      let(:expense) { Expense.new(user: User.new) }
+
+      it "allows access" do
+        expect(subject).to permit(admin, expense)
+      end
+    end
+    context "when the user is not the owner and not an admin" do
+      let(:other_user) { User.new }
+      let(:expense) { Expense.new(user: User.new) }
+
+      it "denies access" do
+        expect(subject).not_to permit(other_user, expense)
+      end
   end
 
   permissions :create? do
